301dfdf6cb7ae959870065188c6fd6575a6454b4
handsomezhou <quanjunzhou@gmail.com>
2013-12-09 01:59:23 +0800

    create alphabet game file

---
+++ 0x01How_to_use_a_callback_function/alphabet_game.c
@@ -0,0 +1,6 @@
+/**
+  *  Copyright (C) 2013-12-09  Handsome Zhou
+  */
+	 
+#include "alphabet_game.h"
+
---
+++ 0x01How_to_use_a_callback_function/alphabet_game.h
@@ -0,0 +1,12 @@
+/**
+  *  Copyright (C) 2013-12-09  Handsome Zhou
+  */
+
+#ifndef ALPHABET_GAME_H
+#define ALPHABET_GAME_H
+
+#define AG_SUCCESS		0
+#define AG_FAILED		(-1)
+
+
+#endif	/*ALPHABET_GAME_H*/
